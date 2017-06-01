73a86d10cfe11d0481cd184a84de346e09629f29
dodo1305 <dodo1305@naver.com>
2017-06-01 19:27:44 +0900

    #5 Button Event Programming

--- app/src/main/java/kr/hs/emirim/dodo1305/simplediarytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dodo1305/simplediarytest/MainActivity.java
@@ -1,15 +1,19 @@
 package kr.hs.emirim.dodo1305.simplediarytest;
 
+import android.content.Context;
 import android.opengl.EGLDisplay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
@@ -24,18 +28,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        date = (DatePicker)findViewById(R.id.date_pick);
-        edit = (EditText)findViewById(R.id.edit);
-        but = (Button)findViewById(R.id.but);
+        date = (DatePicker) findViewById(R.id.date_pick);
+        edit = (EditText) findViewById(R.id.edit);
+        but = (Button) findViewById(R.id.but);
+        but.setOnClickListener(new View.OnClickListener() {
+              @Override
+           public void onClick(View view) {
+                  try {
+                      FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE);
+                      String str = edit.getText().toString();
+                      fOut.write(str.getBytes());
+                      fOut.close();
+                      Toast.makeText(MainActivity.this, "정상적으로 "+ fileName+"파일이 저장되었습니다.", Toast.LENGTH_LONG).show();
+                  } catch (FileNotFoundException e) {
+                      e.printStackTrace();
+                  } catch (IOException e) {
+                      e.printStackTrace();
+                  }
+              }
+      });
 
         Calendar cal = Calendar.getInstance();
-        int year=cal.get(Calendar.YEAR);
-        int month=cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DATE);
         date.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
-                fileName= year+"_"+(month+1)+"_"+day+".txt";
+                fileName = year + "_" + (month + 1) + "_" + day + ".txt";
                 String readData = readDiary(fileName);
                 edit.setText(readData);
                 but.setEnabled(true);
@@ -45,14 +65,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public String readDiary(String fileName){
+
+    public String readDiary(String fileName) {
         String diaryStr = null;
         FileInputStream fin = null;
         try {
             fin = openFileInput(fileName);
             byte[] buf = new byte[500];
             fin.read(buf);
-             diaryStr = new String(buf).trim();
+            diaryStr = new String(buf).trim();
             but.setText("수정 하기");
         } catch (FileNotFoundException e) {
             edit.setText("일기가 존재하지 않습니다.");
@@ -60,6 +81,13 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            fin.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return diaryStr;
     }
+
 }
